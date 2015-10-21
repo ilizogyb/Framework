8fc4b05f5ed9c25056669efa4a14b62ebc9b58a3
unknown <ilizogyb88@gmail.com>
2015-10-21 10:33:53 +0300

    Modification Applications class

--- framework/Application.php
+++ framework/Application.php
@@ -23,7 +23,10 @@ use \Framework\Exception\HTTPNotFoundException;
 
 class Application
 {
-	protected $config;
+    const VIEW_PATH = '/src/Blog/views';
+    protected $config;
+    protected static $request;
+    protected static $router;
 
 	/**
 	* Ініціалізація стану Application
@@ -43,94 +46,51 @@ class Application
 	*/
 	public function run()
 	{
-		//Створення Request і Response
-		$request = new Request;
+        //Створення Request і Response
+		self::$request = new Request;
 		$response = new Response;
-		
-		//Тестування виклику методів Response
-        /*
-		$response->setStatusCode(200,'OK');
-		$response->setHeader('Content-Type','text/html; charset=UTF-8');
-		$response->setCookie('test','Hello from framework',3600);
-		$response->setContent('<h2>Тест встановлення куків</h2>');
-		$response->setContent("<h3><script>document.write(document.cookie);</script></h3>");
-		$response->setContent('<h2>Тест роботи з запитами</h2>');
-		$response->setContent("<form action='' method='POST'><input type='text' name='user'><input type='submit' value='Send'></from>");
-		//echo $response->__toString();
-		$response->send();
-*/
-			
-		//Запуск процесу маршрутизації
+        
+        //Запуск процесу маршрутизації
 		$routes = $this->config['routes'];
-		$router = new Router($request, $routes);
-		$router->run();
-		echo '<br>';
-		echo $router->getController().'</br>';
-        
-        
-       //Створюємо інстанс контроллера
-        //$controllerName = $router->getController();
-        //$controller = new $controllerName ;
-        
-        //Всі запити крім profile в якому декілька дій
-        //if(count($router->getAction() === 1)) {
-          //  $controllerAction = $router->getAction()[0] . 'Action';
-       // }
-        //Запуск на виконання  дій контроллера
-        //call_user_func(array($controller, $controllerAction));
-        
-        
+		self::$router = new Router(self::$request, $routes);
+		self::$router->run();
+		
+        //// Remove this
         echo '<br>';
-		print_r($router->getAction());
-        echo '</br>';
-		echo $router->getMethod().'</br>';
-        echo $router->getSecurity().'</br>';
-		echo $router->getId().'</br>';
-		//echo $router->getSecurity().'</br>';
+		echo self::$router->getController().'</br>';
+        //// Remove this
         
-        //Тест роботи з сесією
-        $session = new Session('userTest');
-        echo $session->start();
-        $session->write('igor','Test');
-        $session->shutdown();
-        $session->start();
-        echo "Session Value: " . $session->read('igor');
-        echo '</br>';
-        echo "Session ID: " . $session->getSessionId();
-        //DI Tests 
+        //Створюємо інстанс контроллера
+        $controllerName = self::$router->getController();
+        $controller = new $controllerName ;
         
-        //aliase
-         //Service::set('test','Framework\DITEst');
-         Service::set('test',function(){
-             $obj = new DITEst;
-             return $obj;
-         });
-         Service::get('test');
-        /*
-        Service::set('Framework\DITEst');
-        $test = Service::get('Framework\DITEst');
-        $test->getText();
-        */
- 
+        //Обробка даних з роутера та виклик потрібного контроллера і передача дій
+        if(count(self::$router->getAction()) === 1) {
+            $controllerAction = self::$router->getAction()[0] . 'Action';
+        } 
 
-       
-       /// Тест контейнера на основі патерна ServiceLocator
-       
-       $serviceContainer = new ServiceLocator;
-       $serviceContainer->set('r', new DITEst);
-       $t = $serviceContainer->get('r');
-       $t->getText();
-       
-       
-       //Тест Exception
-       /*
-       $except = new FrameworkException("Exception test", 505, true);
-       $se = new StorageException("Error stor", 505, true);
-       $ce = new ConfigurationException("Error config", 505, true);
-       $fe = new FileException("File not Found", 505, true);
-       $hnfe = new HTTPNotFoundException("Not found", 404, true);
-       */
+        if(count(self::$router->getAction()) === 2) {
+            if(self::$request->isPost()) {
+                echo $controllerAction = self::$router->getAction()[0] . 'Action';
+            } elseif(self::$request->isGet()) {
+                echo $controllerAction = self::$router->getAction()[1] . 'Action';
+            }
+        }
+        
+        //Запуск на виконання  дій контроллера
+        call_user_func(array($controller, $controllerAction));
+        
 	}
+    
+    /**
+     * Метод для отримання шляху до шаблонів
+     * @return string рядок до папки з шаблонами
+     */
+    public static function getViewPath() 
+    {
+      return $path = $_SERVER['DOCUMENT_ROOT'] . self::VIEW_PATH;
+       
+    }
 
 
 }
